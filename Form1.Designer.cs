1d31a1466bdf2d53ef421678d31de7ad18aa2ecc
adamoroff <36341243+adamoroff@users.noreply.github.com>
2019-04-30 16:41:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Label12 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.WebBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Label11 = new System.Windows.Forms.Label();
+            this.Label10 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label9 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(20, 273);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(313, 36);
+            this.Button2.TabIndex = 22;
+            this.Button2.Text = "Get";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Label12
+            // 
+            this.Label12.AutoSize = true;
+            this.Label12.Location = new System.Drawing.Point(145, 225);
+            this.Label12.Name = "Label12";
+            this.Label12.Size = new System.Drawing.Size(13, 13);
+            this.Label12.TabIndex = 20;
+            this.Label12.Text = "--";
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Location = new System.Drawing.Point(22, 225);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(52, 13);
+            this.Label7.TabIndex = 21;
+            this.Label7.Text = "Currency:";
+            // 
+            // WebBrowser1
+            // 
+            this.WebBrowser1.Location = new System.Drawing.Point(359, 12);
+            this.WebBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser1.Name = "WebBrowser1";
+            this.WebBrowser1.Size = new System.Drawing.Size(792, 591);
+            this.WebBrowser1.TabIndex = 19;
+            // 
+            // Label11
+            // 
+            this.Label11.AutoSize = true;
+            this.Label11.Location = new System.Drawing.Point(145, 200);
+            this.Label11.Name = "Label11";
+            this.Label11.Size = new System.Drawing.Size(13, 13);
+            this.Label11.TabIndex = 17;
+            this.Label11.Text = "--";
+            // 
+            // Label10
+            // 
+            this.Label10.AutoSize = true;
+            this.Label10.Location = new System.Drawing.Point(145, 175);
+            this.Label10.Name = "Label10";
+            this.Label10.Size = new System.Drawing.Size(13, 13);
+            this.Label10.TabIndex = 15;
+            this.Label10.Text = "--";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(22, 200);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(91, 13);
+            this.Label6.TabIndex = 18;
+            this.Label6.Text = "Internet Provider: ";
+            // 
+            // Label9
+            // 
+            this.Label9.AutoSize = true;
+            this.Label9.Location = new System.Drawing.Point(145, 150);
+            this.Label9.Name = "Label9";
+            this.Label9.Size = new System.Drawing.Size(13, 13);
+            this.Label9.TabIndex = 10;
+            this.Label9.Text = "--";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(22, 175);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(27, 13);
+            this.Label5.TabIndex = 16;
+            this.Label5.Text = "City:";
+            // 
+            // Label8
+            // 
+            this.Label8.AutoSize = true;
+            this.Label8.Location = new System.Drawing.Point(145, 125);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(13, 13);
+            this.Label8.TabIndex = 11;
+            this.Label8.Text = "--";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(22, 150);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(47, 13);
+            this.Label4.TabIndex = 12;
+            this.Label4.Text = "Region: ";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(22, 125);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(49, 13);
+            this.Label3.TabIndex = 13;
+            this.Label3.Text = "Country: ";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(20, 70);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(83, 13);
+            this.Label2.TabIndex = 14;
+            this.Label2.Text = "Enter IP Adress:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label1.Location = new System.Drawing.Point(15, 13);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(75, 26);
+            this.Label1.TabIndex = 9;
+            this.Label1.Text = "IP Info";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(148, 67);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(185, 20);
+            this.TextBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 617);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Label12);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.WebBrowser1);
+            this.Controls.Add(this.Label11);
+            this.Controls.Add(this.Label10);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label9);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label8);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Label Label12;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.WebBrowser WebBrowser1;
+        internal System.Windows.Forms.Label Label11;
+        internal System.Windows.Forms.Label Label10;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label9;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextBox1.KeyDown += (s, e) =>
+            {
+                if (e.KeyData == Keys.Return)
+                {
+                    Button2_Click(null, null);
+                }
+            };
+
+            TextBox1.TextChanged += (s, e) =>
+            {
+                if (Regex.IsMatch(TextBox1.Text, "[^0-9-.]"))
+                    TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1);
+                TextBox1.SelectionStart = TextBox1.Text.Length;
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //887; 491 def
+            Size = new Size(370, 364);
+        }
+
+        async void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Enabled = false;
+            Button2.Enabled = false;
+            await Task.Run(() => {
+                WebClient wb = new WebClient();
+            string line = wb.DownloadString($"http://free.ipwhois.io/xml/{TextBox1.Text}");
+            Debug.WriteLine(line);
+            if (!line.Contains("invalid"))
+            {
+                Action action = () => {
+                    Match match = Regex.Match(line, "<country>(.*?)</country>(.*?)<region>(.*?)</region>(.*?)<city>(.*?)</city>(.*?)<org>(.*?)</org>(.*?)<currency>(.*?)</currency>");
+                    Match matchloc = Regex.Match(line, "<latitude>(.*?)</latitude>(.*?)<longitude>(.*?)</longitude>");
+                    Label8.Text = match.Groups[1].ToString();
+                    Label9.Text = match.Groups[3].ToString();
+                    Label10.Text = match.Groups[5].ToString();
+                    Label11.Text = match.Groups[7].ToString();
+                    Label12.Text = match.Groups[9].ToString();
+                    string latitude = matchloc.Groups[1].ToString();
+                    string longitude = matchloc.Groups[3].ToString();
+                    Size = new Size(1180, 656);
+                    WebBrowser1.Url = new Uri($"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}");
+                    //maps(latitude, longitude);
+                    Debug.WriteLine(latitude + ":" + longitude);
+                };
+
+                if (InvokeRequired)
+                    Invoke(action);
+                else
+                    action();
+                    
+            }
+            else
+            {
+                MessageBox.Show("Загугли, как выглядит IP");
+                }
+            });
+            TextBox1.Enabled = true;
+            Button2.Enabled = true;
+        }
+
+        /*async void maps(string latitude, string longitude)
+        {
+            await Task.Run(() => {
+                WebBrowser1.Url = new Uri($"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}");
+            });
+        }*/
+    }
+}
